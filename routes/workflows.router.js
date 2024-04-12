d40a095ac06fd72fac6c59d1e2c00dad748b1124
Ali Khader <120788843+Blacklacan@users.noreply.github.com>
2024-04-12 15:03:48 +0300

    Update workflows.router.js

--- routes/workflows.router.js
+++ routes/workflows.router.js
@@ -1,1 +1,12 @@
+const express = require("express")
+const router = express.Router()
 
+const workflowsController = require("../controller/posts.controller")
+
+router.get("/", workflowsController.getAll)
+router.get("/:id", workflowsController.getById)
+router.post("/", workflowsController.create)
+router.put("/:id", workflowsController.update)
+router.delete("/:id", workflowsController.delete)
+
+module.exports = router
